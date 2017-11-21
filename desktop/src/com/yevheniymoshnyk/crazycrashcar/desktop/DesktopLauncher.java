18461b387b42d9e36225561ccb5ebcc2108e7f24
Evgeniy Moshnyk <fly123@meta.ua>
2017-11-21 14:08:33 +0200

    Create project (push)

---
+++ desktop/src/com/yevheniymoshnyk/crazycrashcar/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.yevheniymoshnyk.crazycrashcar.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.yevheniymoshnyk.crazycrashcar.CrazyCrashCar;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CrazyCrashCar(), config);
+	}
+}
